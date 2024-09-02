d80b7566f7a98c8fa5e07ce8adba0af21c49bc4e
kotan4ik <kostyuk.max@gmail.com>
2024-09-02 18:12:22 +0800

    feat: tests to delete courier end point added

---
+++ src/test/java/com/kotan4ik/tests/DeleteCourierTest.java
@@ -0,0 +1,49 @@
+package com.kotan4ik.tests;
+
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.kotan4ik.requests.CourierApiRequests.*;
+import static com.kotan4ik.utils.ApiErrorMessages.COURIER_ID_NOT_FOUND;
+import static com.kotan4ik.utils.ApiErrorMessages.DELETE_NOT_ENOUGH_DATA;
+import static com.kotan4ik.utils.Assertions.*;
+
+
+public class DeleteCourierTest {
+
+    private static final String CORRECT_LOGIN = "myOriginalLogin";
+    private static final String CORRECT_PASSWORD = "myPassword";
+    private static final String CORRECT_NAME = "originalName";
+    private int courierId;
+
+    @BeforeEach
+    public void setUp() {
+        createCourier(CORRECT_LOGIN, CORRECT_PASSWORD, CORRECT_NAME);
+        Response response = loginCourier(CORRECT_LOGIN, CORRECT_PASSWORD);
+        courierId = Integer.parseInt(response.path("id").toString());
+    }
+
+    @Test
+    public void positiveTestShouldReturn200Ok() {
+        Response response = deleteCourier(courierId);
+        compareResponseCode(response, HttpStatus.SC_OK);
+        isSuccessfulResponseBody(response);
+    }
+
+    @Test
+    public void negativeTestWithoutIdShouldReturn400BadRequest() {
+        Response response = deleteCourier();
+        compareResponseCode(response, 400);
+        isErrorMessageCorrect(response, DELETE_NOT_ENOUGH_DATA);
+    }
+
+    @Test
+    public void negativeTestShouldReturn404NotFound() {
+        deleteCourier(courierId);
+        Response response = deleteCourier(courierId);
+        compareResponseCode(response, HttpStatus.SC_NOT_FOUND);
+        isErrorMessageCorrect(response, COURIER_ID_NOT_FOUND);
+    }
+}
